8a95fd57f4d981f86ba4d74c883b47230e91a6cb
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-02 12:18:16 +0530

    :construction: Add last commit helper

--- src/git.ts
+++ src/git.ts
@@ -5,6 +5,10 @@ export const commit = (message: string) => {
   exec(`git commit -m "${message.replace(/\"/g, "''")}"`);
 };
 
-export const push = (message: string) => {
+export const push = () => {
   exec("git push");
+};
+
+export const lastCommit = () => {
+  return exec(`git log --format="%H" -n 1`).stdout;
 };
