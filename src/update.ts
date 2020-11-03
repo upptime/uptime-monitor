d036dd96fe7ad7fb588dd3ab0116332c997fa95c
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-03 11:44:31 +0530

    :bug: Use plain text replace

--- src/update.ts
+++ src/update.ts
@@ -98,13 +98,13 @@ export const update = async (shouldCommit = false) => {
             (config.commitMessages || {}).statusChange ||
             "$EMOJI $SITE_NAME is $STATUS ($RESPONSE_CODE in $RESPONSE_TIME ms) [skip ci] [upptime]"
           )
-            .replace(new RegExp("$EMOJI", "g"), status === "up" ? "🟩" : "🟥")
-            .replace(new RegExp("$SITE_NAME", "g"), site.name)
-            .replace(new RegExp("$SITE_URL", "g"), site.url)
-            .replace(new RegExp("$SITE_METHOD", "g"), site.method || "GET")
-            .replace(new RegExp("$STATUS", "g"), status)
-            .replace(new RegExp("$RESPONSE_CODE", "g"), result.httpCode.toString())
-            .replace(new RegExp("$RESPONSE_TIME", "g"), responseTime),
+            .replace("$EMOJI", status === "up" ? "🟩" : "🟥")
+            .replace("$SITE_NAME", site.name)
+            .replace("$SITE_URL", site.url)
+            .replace("$SITE_METHOD", site.method || "GET")
+            .replace("$STATUS", status)
+            .replace("$RESPONSE_CODE", result.httpCode.toString())
+            .replace("$RESPONSE_TIME", responseTime),
           (config.commitMessages || {}).commitAuthorName,
           (config.commitMessages || {}).commitAuthorEmail
         );
