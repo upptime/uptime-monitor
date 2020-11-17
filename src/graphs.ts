c6f637618a1b4573be894bbf4f26696d6002d398
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-17 11:27:12 +0530

    :bug: Add temporary bug fix (upptime/upptime#56)

--- src/graphs.ts
+++ src/graphs.ts
@@ -4,11 +4,13 @@ import { join } from "path";
 import { exec } from "shelljs";
 import { commit, push } from "./git";
 import { UpptimeConfig } from "./interfaces";
+import { tempFixes } from "./temp";
 
 export const generateGraphs = async () => {
   const config = safeLoad(await readFile(join(".", ".upptimerc.yml"), "utf8")) as UpptimeConfig;
   exec("npx @upptime/graphs");
   exec("npx imagemin-cli graphs/* --out-dir=graphs");
+  await tempFixes();
   commit(
     (config.commitMessages || {}).graphsUpdate || ":bento: Update graphs [skip ci]",
     (config.commitMessages || {}).commitAuthorName,
