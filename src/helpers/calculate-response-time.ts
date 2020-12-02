a1a9cfb44bca1bda407252795293898654e180f0
Anand Chowdhary <mail@anandchowdhary.com>
2020-12-02 22:45:32 +0530

    :bug: Round response time to int

--- src/helpers/calculate-response-time.ts
+++ src/helpers/calculate-response-time.ts
@@ -67,11 +67,11 @@ export const getResponseTimeForSite = async (
     : "down";
 
   return {
-    day: avg(daySum) || 0,
-    week: avg(weekSum) || 0,
-    month: avg(monthSum) || 0,
-    year: avg(yearSum) || 0,
-    all: avg(allSum) || 0,
+    day: Math.round(avg(daySum) || 0),
+    week: Math.round(avg(weekSum) || 0),
+    month: Math.round(avg(monthSum) || 0),
+    year: Math.round(avg(yearSum) || 0),
+    all: Math.round(avg(allSum) || 0),
     currentStatus,
   };
 };
