154bcec18b8b8ab73538113b8cd4df4a75a67130
Anand Chowdhary <github@anandchowdhary.com>
2021-03-25 12:15:49 +0530

    :bug: Add GH_PAT to Octokit

--- src/helpers/github.ts
+++ src/helpers/github.ts
@@ -4,7 +4,7 @@ import { getConfig } from "./config";
 export const getOctokit = async (): Promise<Octokit> => {
   const config = await getConfig();
   return new Octokit({
-    auth: config.PAT || process.env.GITHUB_TOKEN,
+    auth: config.PAT || process.env.GH_PAT || process.env.GITHUB_TOKEN,
     userAgent: config["user-agent"] || process.env.USER_AGENT || "KojBot",
   });
 };
