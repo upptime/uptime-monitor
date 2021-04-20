e1a90da0159c6c2791913ab906e3685a62ab6f15
Anand Chowdhary <github@anandchowdhary.com>
2021-04-21 02:56:50 +0530

    :recycle: Rename default UA (fixed upptime/upptime#320)

--- src/helpers/github.ts
+++ src/helpers/github.ts
@@ -6,6 +6,6 @@ export const getOctokit = async (): Promise<Octokit> => {
   const config = await getConfig();
   return new Octokit({
     auth: config.PAT || getSecret("GH_PAT") || getSecret("GITHUB_TOKEN"),
-    userAgent: config["user-agent"] || getSecret("USER_AGENT") || "KojBot",
+    userAgent: config["user-agent"] || getSecret("USER_AGENT") || "upptime",
   });
 };
