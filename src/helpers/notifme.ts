1a286bd38755e4770e8a03c9499c3514c779e55d
Restent Ou <53113178+gxres042@users.noreply.github.com>
2024-05-06 01:47:09 +0800

    :sparkles: Add support for multiple Telegram channels (#249)
    * Add support for multiple NOTIFICATION_TELEGRAM_CHAT_ID
    
    * Update src/helpers/notifme.ts
    
    * Update src/helpers/notifme.ts
    
    * Update src/helpers/notifme.ts
    
    * MY MISTAKE: BOT_KEY => CHAT_ID
    
    ---------
    
    Co-authored-by: Anand Chowdhary <github@anandchowdhary.com>

--- src/helpers/notifme.ts
+++ src/helpers/notifme.ts
@@ -331,15 +331,18 @@ export const sendNotification = async (message: string) => {
   if (getSecret("NOTIFICATION_TELEGRAM") && getSecret("NOTIFICATION_TELEGRAM_BOT_KEY")) {
     console.log("Sending Telegram");
     try {
-      await axios.post(
-        `https://api.telegram.org/bot${getSecret("NOTIFICATION_TELEGRAM_BOT_KEY")}/sendMessage`,
-        {
-          parse_mode: "Markdown",
-          disable_web_page_preview: true,
-          chat_id: getSecret("NOTIFICATION_TELEGRAM_CHAT_ID"),
-          text: message.replace(/_/g, '\\_'),
-        }
-      );
+      const chatIds = getSecret("NOTIFICATION_TELEGRAM_CHAT_ID").split(",");
+      for (const chatId of chatIds) {
+        await axios.post(
+          `https://api.telegram.org/bot${getSecret("NOTIFICATION_TELEGRAM_BOT_KEY")}/sendMessage`,
+          {
+            parse_mode: "Markdown",
+            disable_web_page_preview: true,
+            chat_id: chatId.trim(),
+            text: message.replace(/_/g, '\\_'),
+          }
+        );
+      }
       console.log("Success Telegram");
     } catch (error) {
       console.log("Got an error", error);
